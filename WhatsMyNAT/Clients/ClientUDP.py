63d0267fdb51e1316e7cd8f154798351b6d53f6a
iAmGroute <iAmGroute@outlook.com>
2019-03-24 12:53:19 +0200

    UDP initial commit

---
+++ WhatsMyNAT/Clients/ClientUDP.py
@@ -0,0 +1,62 @@
+
+import sys
+import logging
+import socket
+
+log = logging.getLogger(__name__)
+
+class ClientUDP:
+
+    def __init__(self, port, address='0.0.0.0'):
+        self.port    = port
+        self.address = address
+        log.info('Local address and port: [{0}]:{1}'.format(address, port))
+
+    def getAddressFrom(self, serverAddr, serverPort):
+        log.info('Connecting to server  : [{0}]:{1}'.format(serverAddr, serverPort))
+
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((self.address, self.port))
+            s.sendto(b'', (serverAddr, serverPort))
+            reply = s.recvfrom(1024)
+
+        log.info('Got reply from server : {0}'.format(reply))
+
+        reply = reply.decode('utf-8').split('\n')
+        externalAddr = reply[0]
+        externalPort = int(reply[1])
+        return externalAddr, externalPort
+
+
+def main(serverAddr, serverPort, port, address):
+    logging.basicConfig(format='%(created).3f [%(levelname)s] %(message)s', level=logging.INFO)
+
+    clientUDP = ClientUDP(port, address)
+    externalAddr, externalPort = clientUDP.getAddressFrom(serverAddr, serverPort)
+
+    log.info('External address and port: [{0}]:{1}'.format(externalAddr, externalPort))
+
+
+def parseArgs(args):
+    serverAddr = args[1]
+
+    serverPort = int(args[2])
+    assert 0 < serverPort < 65536
+
+    port = int(args[3])
+    assert 0 < port < 65536
+
+    address = '0.0.0.0'
+    if len(args) > 4:
+        address = args[4]
+
+    return serverAddr, serverPort, port, address
+
+if __name__ == '__main__':
+    try:
+        config = parseArgs(sys.argv)
+    except Exception as e:
+        print('Usage: python3 ClientUDP.py <serverAddr> <serverPort> <localPort> [<nicAddress>]')
+    else:
+        main(*config)
---
+++ WhatsMyNAT/Servers/ServerUDP.py
@@ -0,0 +1,60 @@
+
+import sys
+import logging
+import socket
+
+log = logging.getLogger(__name__)
+
+class ServerUDP:
+
+    def __init__(self, port, address='0.0.0.0'):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((address, port))
+        log.info('Server started on [{0}]:{1}'.format(address, port))
+
+    def stop(self):
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
+        log.info('Server stopped')
+
+    def task(self):
+        data, addr = self.socket.recvfrom(1024)
+        log.info('Connection from: [{0}]:{1}'.format(*addr))
+        reply = '{0}\n{1}\n'.format(*addr)
+        self.socket.sendto(bytes(reply, 'utf-8'), addr)
+
+
+def main(port, address):
+    logging.basicConfig(format='%(created).3f [%(levelname)s] %(message)s', level=logging.INFO)
+
+    serverUDP = ServerUDP(port, address)
+
+    while True:
+        try:
+            serverUDP.task()
+
+        except KeyboardInterrupt:
+            log.info('Exit requested by keyboard')
+            break
+
+        except Exception as e:
+            log.exception(e)
+
+
+def parseArgs(args):
+    port = int(args[1])
+    assert 0 < port < 65536
+
+    address = '0.0.0.0'
+    if len(args) > 2:
+        address = args[2]
+
+    return port, address
+
+if __name__ == '__main__':
+    try:
+        config = parseArgs(sys.argv)
+    except Exception as e:
+        print('Usage: python3 ServerUDP.py <port> [<nicAddress>]')
+    else:
+        main(*config)
