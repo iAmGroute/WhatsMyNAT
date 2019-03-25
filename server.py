cc31f026add0e19a3232a10b86f63c9651bbf762
iAmGroute <pgonis@gmail.com>
2019-03-25 21:18:26 +0200

    Combined server TCP and UDP cli files

--- server.py
+++ server.py
@@ -3,12 +3,13 @@ import sys
 import logging
 import socket
 
+from WhatsMyNAT.ServerTCP import ServerTCP
 from WhatsMyNAT.ServerUDP import ServerUDP
 
 log = logging.getLogger(__name__)
 
-def main(port, address, counterpart, endpointC):
-    serverUDP = ServerUDP(port, address, counterpart, endpointC)
+def main(serverClass, port, address, counterpart, endpointC):
+    serverUDP = serverClass(port, address, counterpart, endpointC)
 
     while True:
         try:
@@ -26,36 +27,42 @@ def main(port, address, counterpart, endpointC):
 
 
 def parseArgs(args):
-    port = int(args[1])
+    mode = args[1].upper()
+    if mode == 'TCP':
+        serverClass = ServerTCP
+    elif mode == 'UDP':
+        serverClass = ServerUDP
+
+    port = int(args[2])
     assert 0 < port < 65536
 
     address = '0.0.0.0'
-    if len(args) > 2:
-        address = args[2]
+    if len(args) > 3:
+        address = args[3]
 
     counterpart = None
-    if len(args) > 4:
-        cAddress = args[3]
-        cPort    = int(args[4])
+    if len(args) > 5:
+        cAddress = args[4]
+        cPort    = int(args[5])
         assert 0 < cPort < 65536
         counterpart = (cAddress, cPort)
 
     cePort = 0
-    if len(args) > 5:
-        cePort = int(args[5])
+    if len(args) > 6:
+        cePort = int(args[6])
         assert 0 <= cePort < 65536
 
     ceAddress = '0.0.0.0'
-    if len(args) > 6:
-        ceAddress = args[6]
+    if len(args) > 7:
+        ceAddress = args[7]
 
-    return port, address, counterpart, (ceAddress, cePort)
+    return serverClass, port, address, counterpart, (ceAddress, cePort)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(created).3f [%(name)s|%(levelname)s] %(message)s', level=logging.INFO)
     try:
         config = parseArgs(sys.argv)
     except Exception as e:
-        print('Usage: python3 serverUDP.py <port> [<nicAddress>] [<cAddress> <cPort>] [<cePort>] [<ceAddress>]')
+        print('Usage: python3 server.py [TCP|UDP] <port> [<nicAddress>] [<cAddress> <cPort>] [<cePort>] [<ceAddress>]')
     else:
         main(*config)
