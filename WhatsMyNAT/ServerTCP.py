b133c93f76f9abdc93d0f9463dc998f56114ac84
iAmGroute <pgonis@gmail.com>
2019-03-25 21:14:09 +0200

    ServerTCP: Added Counterpart support

--- WhatsMyNAT/ServerTCP.py
+++ WhatsMyNAT/ServerTCP.py
@@ -4,16 +4,40 @@ import socket
 
 from .Common.Connector import Connector
 
-log = logging.getLogger(__name__)
+log  = logging.getLogger(__name__)
+logC = logging.getLogger(__name__ + ':C')
 
 class ServerTCP:
 
-    def __init__(self, port, address='0.0.0.0'):
-        self.con = Connector(log, socket.SOCK_STREAM, None, port, address)
+    def __init__(self, port, address='0.0.0.0', counterpart=None, endpointC=None):
+        self.counterpart = counterpart
+        self.con         = Connector(log, socket.SOCK_STREAM, None, port, address)
         self.con.listen()
+        self.conC        = None
+        if counterpart:
+            log.info('    with counterpart [{0}]:{1}'.format(*counterpart))
+            if endpointC:
+                log.info('    using [{0}]:{1}'.format(*endpointC))
+                self.conC = Connector(logC, socket.SOCK_DGRAM, 2, endpointC[1], endpointC[0])
+            else:
+                self.conC = Connector(logC, socket.SOCK_DGRAM, 2, 0, self.address)
 
     def task(self):
         conn, addr = self.con.accept()
+        reply = '{0}\n{1}\n'.format(*addr)
         with conn:
-            reply = '{0}\n{1}\n'.format(*addr)
+            conn.settimeout(0.2)
+            try:
+                data = conn.recv(64)
+            except socket.timeout:
+                pass
+            else:
+                if len(data) == 16:
+                    # Received token for reverse testing
+                    log.info('    with token : {0}'.format(data))
+                    if self.conC:
+                        for _ in range(3):
+                            self.conC.sendto(data + bytes(reply, 'utf-8'), self.counterpart)
+                    # TODO: append counterpart's probing address and port
+                    #reply +=
             conn.sendall(bytes(reply, 'utf-8'))
