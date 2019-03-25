9ab5391fbe167807344d940721572b8ecdceb792
iAmGroute <pgonis@gmail.com>
2019-03-25 20:50:40 +0200

    followup

--- WhatsMyNAT/ClientTCP.py
+++ WhatsMyNAT/ClientTCP.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 class ClientTCP:
 
     def __init__(self, port, address='0.0.0.0'):
-        self.con = Connector(log, socket.SOCK_STREAM, 2, self.port, self.address)
+        self.con = Connector(log, socket.SOCK_STREAM, 2, port, address)
 
     def __enter__(self):
         return self
--- WhatsMyNAT/ClientUDP.py
+++ WhatsMyNAT/ClientUDP.py
@@ -11,7 +11,7 @@ log = logging.getLogger(__name__)
 class ClientUDP:
 
     def __init__(self, port, address='0.0.0.0'):
-        self.con = Connector(log, socket.SOCK_DGRAM, 2, self.port, self.address)
+        self.con = Connector(log, socket.SOCK_DGRAM, 2, port, address)
 
     def __enter__(self):
         return self
