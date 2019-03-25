fac761a8b700fc80a92a24b00262acccefa283aa
iAmGroute <pgonis@gmail.com>
2019-03-25 20:48:49 +0200

    Separated client's implementation and cli code

--- WhatsMyNAT/ClientTCP.py
+++ WhatsMyNAT/ClientTCP.py
@@ -1,5 +1,4 @@
 
-import sys
 import logging
 import socket
 
@@ -28,36 +27,3 @@ class ClientTCP:
         externalAddr = reply[0]
         externalPort = int(reply[1])
         return externalAddr, externalPort
-
-
-def main(serverAddr, serverPort, port, address):
-    logging.basicConfig(format='%(created).3f [%(levelname)s] %(message)s', level=logging.INFO)
-
-    clientTCP = ClientTCP(port, address)
-    externalAddr, externalPort = clientTCP.getAddressFrom(serverAddr, serverPort)
-
-    log.info('External address and port: [{0}]:{1}'.format(externalAddr, externalPort))
-
-
-def parseArgs(args):
-    serverAddr = args[1]
-
-    serverPort = int(args[2])
-    assert 0 < serverPort < 65536
-
-    port = int(args[3])
-    assert 0 < port < 65536
-
-    address = '0.0.0.0'
-    if len(args) > 4:
-        address = args[4]
-
-    return serverAddr, serverPort, port, address
-
-if __name__ == '__main__':
-    try:
-        config = parseArgs(sys.argv)
-    except Exception as e:
-        print('Usage: python3 ClientTCP.py <serverAddr> <serverPort> <localPort> [<nicAddress>]')
-    else:
-        main(*config)
---
+++ client.py
@@ -0,0 +1,45 @@
+
+import sys
+import logging
+
+from WhatsMyNAT.ClientTCP import ClientTCP
+from WhatsMyNAT.ClientUDP import ClientUDP
+
+log = logging.getLogger(__name__)
+
+def main(clientClass, serverAddr, serverPort, port, address):
+    with clientClass(port, address) as client:
+        externalAddr, externalPort = clientTCP.getAddressFrom(serverAddr, serverPort)
+
+    log.info('External address and port: [{0}]:{1}'.format(externalAddr, externalPort))
+
+
+def parseArgs(args):
+    mode = args[1].upper()
+    if mode == 'TCP':
+        clientClass = ClientTCP
+    elif mode == 'UDP':
+        clientClass = ClientUDP
+
+    serverAddr = args[2]
+
+    serverPort = int(args[3])
+    assert 0 < serverPort < 65536
+
+    port = int(args[4])
+    assert 0 < port < 65536
+
+    address = '0.0.0.0'
+    if len(args) > 5:
+        address = args[5]
+
+    return clientClass, serverAddr, serverPort, port, address
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(created).3f [%(name)s|%(levelname)s] %(message)s', level=logging.INFO)
+    try:
+        config = parseArgs(sys.argv)
+    except Exception as e:
+        print('Usage: python3 client.py [TCP|UDP] <serverAddr> <serverPort> <localPort> [<nicAddress>]')
+    else:
+        main(*config)
