a623fbfee144c5ca72fb575a37f313408a27fda4
iAmGroute <pgonis@gmail.com>
2019-03-29 14:49:27 +0200

    Servers update followup: fixed server.py

--- server.py
+++ server.py
@@ -7,8 +7,8 @@ from WhatsMyNAT.ServerUDP import ServerUDP
 
 log = logging.getLogger(__name__)
 
-def main(serverClass, port, address, counterpart, endpointC):
-    serverUDP = serverClass(port, address, counterpart, endpointC)
+def main(serverClass, port, address, probePort, counterpart, endpointC):
+    serverUDP = serverClass(port, address, probePort, counterpart, endpointC)
 
     while True:
         try:
@@ -36,29 +36,34 @@ def parseArgs(args):
     if len(args) > 3:
         address = args[3]
 
+    probePort = 0
+    if len(args) > 4:
+        probePort = int(args[4])
+        assert 0 <= probePort < 65536
+
     counterpart = None
-    if len(args) > 5:
-        cAddress = args[4]
-        cPort    = int(args[5])
+    if len(args) > 6:
+        cAddress = args[5]
+        cPort    = int(args[6])
         assert 0 < cPort < 65536
         counterpart = (cAddress, cPort)
 
     cePort = 0
-    if len(args) > 6:
-        cePort = int(args[6])
+    if len(args) > 7:
+        cePort = int(args[7])
         assert 0 <= cePort < 65536
 
     ceAddress = '0.0.0.0'
-    if len(args) > 7:
-        ceAddress = args[7]
+    if len(args) > 8:
+        ceAddress = args[8]
 
-    return serverClass, port, address, counterpart, (ceAddress, cePort)
+    return serverClass, port, address, probePort, counterpart, (ceAddress, cePort)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(created).3f [%(name)s|%(levelname)s] %(message)s', level=logging.INFO)
     try:
         config = parseArgs(sys.argv)
     except Exception as e:
-        print('Usage: python3 server.py [TCP|UDP] <port> [<nicAddress>] [<cAddress> <cPort>] [<cePort>] [<ceAddress>]')
+        print('Usage: python3 server.py [TCP|UDP] <port> [<nicAddress>] [<probePort>] [<cAddress> <cPort>] [<cePort>] [<ceAddress>]')
     else:
         main(*config)
