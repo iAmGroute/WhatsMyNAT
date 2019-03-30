32079e0c3cca6dae35464edb0808451b311a795a
iAmGroute <pgonis@gmail.com>
2019-03-30 02:47:28 +0200

    restrict: working implementation

--- restrict.py
+++ restrict.py
@@ -1,6 +1,7 @@
 
 import sys
 import random
+from collections import Counter
 
 from names import *
 from WhatsMyNAT.ClientTCP import ClientTCP
@@ -19,45 +20,70 @@ def runTest(clientClass, port, address):
     for i in range(len(servers)):
         dAddress, dPorts = servers[i]
 
-        replies = []
+        result = []
         for j in range(len(dPorts)):
-            dPort = dPorts[j]
-
-            print(' {0:2d} | {1:5d} | {2:38}  '.format(i, dPort, dAddress), end='')
+            print(' {0:2d} | {1:5d} | {2:38}  '.format(i, dPorts[j], dAddress), end='')
 
             with clientClass(port, address) as client:
-                reply = client.getRepliesFrom(dAddress, dPort)
+                replies = client.getRepliesFrom(dAddress, dPorts[j])
 
-            print('OK' if reply else 'ERROR')
-            replies.append(reply)
+            if replies:
+                print('OK')
+                sources = [reply[1] for reply in replies]
+                result.append(list(Counter(sources).items()))
+            else:
+                print('ERROR')
+                result.append(None)
 
-        results.append(replies)
+        results.append(result)
 
-    print('-------- Results ---------------------')
-    print('  Server    |   reply received from   ')
-    print(' id |  port |     address     |  port ')
-    print('----|-------|-----------------|-------')
-    #     ' 35 | 12345 | 123.123.123.123 | 54321 '
+    print('-------- Results -------------------------')
+    print('  Server    |  replies received from      ')
+    print(' id |  port | # |     address     |  port ')
+    print('----|-------|---|-----------------|-------')
+    #     ' 35 | 12345 | 3 | 123.123.123.123 | 54321 '
     for i in range(len(servers)):
-        dAddress, dPorts = servers[i]
-        replies          = results[i]
-        for j in range(len(dPorts)):
-            dPort = dPorts[j]
-            for reply, ep in replies[j]:
-                print(' {0:2d} | {1:5d} | {2:15} | {3:5d}'.format(i, dPort, ep[0], ep[1]))
+        for j in range(len(servers[i][1])):
+            for source, count in results[i][j] or []:
+                print(' {0:2d} | {1:5d} | {2:1d} | {3:15} | {4:5d}'.format(i, servers[i][1][j], count, source[0], source[1]))
 
-    return Permissiveness.undetermined
+    print('----- Summaries ------')
+    print('  Server    | unique  ')
+    print(' id |  port | replies ')
+    print('----|-------|---------')
+    #     ' 35 | 12345 |    3    '
+    summaries = []
+    for i in range(len(servers)):
+        for j in range(len(servers[i][1])):
+            uc = len(results[i][j] or [])
+            summaries.append(uc)
+            print(' {0:2d} | {1:5d} |    {2:1d}'.format(i, servers[i][1][j], uc))
+
+    temp = Counter(summaries)
+    uc, frequency = temp.most_common(1)[0]
+    frequency = 100 * frequency / (len(summaries) - temp[0])
+
+    if uc >= 3:
+        restrict = Restrictiveness.permissive
+    elif uc == 2:
+        restrict = Restrictiveness.ipRestricted
+    elif uc == 1:
+        restrict = Restrictiveness.portRestricted
+    else:
+        restrict = Restrictiveness.undetermined
+
+    return restrict, frequency
 
 
 def main(portTCP, portUDP, address):
-    print('Running TCP permissiveness test')
-    senseTCP = runTest(ClientTCP, portTCP, address)
+    print('Running TCP restrictiveness test')
+    senseTCP, fTCP = runTest(ClientTCP, portTCP, address)
     print()
-    print('Running UDP permissiveness test')
-    senseUDP = runTest(ClientUDP, portUDP, address)
+    print('Running UDP restrictiveness test')
+    senseUDP, fUDP = runTest(ClientUDP, portUDP, address)
     print()
-    print('TCP permissiveness: ' + str(senseTCP))
-    print('UDP permissiveness: ' + str(senseUDP))
+    print('TCP restrictiveness: {0} | concistency: {1:3d}%'.format(senseTCP, round(fTCP)))
+    print('UDP restrictiveness: {0} | concistency: {1:3d}%'.format(senseUDP, round(fUDP)))
 
 
 def parseArgs(args):
