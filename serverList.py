f019df463d6486ca2a0d898beeea4b5d1ac9b939
iAmGroute <pgonis@gmail.com>
2019-03-31 14:07:29 +0300

    Added serverList

---
+++ serverList.py
@@ -0,0 +1,6 @@
+
+servers = [
+    ('groute0.westeurope.cloudapp.azure.com',  [1234, 1235]),
+    ('groute1.westeurope.cloudapp.azure.com',  [1234, 1235]),
+    ('groute2.westeurope.cloudapp.azure.com',  [1234, 1235]),
+]
