42ad01c589efb15e1b98099a9802bd18c3b269c5
iAmGroute <pgonis@gmail.com>
2019-03-31 21:00:53 +0300

    new Server ports

--- serverList.py
+++ serverList.py
@@ -1,6 +1,6 @@
 
 servers = [
-    ('groute0.westeurope.cloudapp.azure.com',  [1234, 1235]),
-    ('groute1.westeurope.cloudapp.azure.com',  [1234, 1235]),
-    ('groute2.westeurope.cloudapp.azure.com',  [1234, 1235]),
+    ('groute0.westeurope.cloudapp.azure.com',  [1260, 1261, 1262, 1263]),
+    ('groute1.westeurope.cloudapp.azure.com',  [1260, 1261, 1262, 1263]),
+    ('groute2.westeurope.cloudapp.azure.com',  [1260, 1261, 1262, 1263]),
 ]
