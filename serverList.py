df8e96260b6d0be90f4b44a067a9604f76955954
Groute <iAmGroute@outlook.com>
2019-04-03 23:11:10 +0300

    Update serverList.py

--- serverList.py
+++ serverList.py
@@ -2,5 +2,4 @@
 servers = [
     ('groute0.westeurope.cloudapp.azure.com',  [1260, 1261, 1262, 1263]),
     ('groute1.westeurope.cloudapp.azure.com',  [1260, 1261, 1262, 1263]),
-    ('groute2.westeurope.cloudapp.azure.com',  [1260, 1261, 1262, 1263]),
 ]
